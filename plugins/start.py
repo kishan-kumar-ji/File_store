f7696f21e903cd97181b56ea5e611a9254d766f1
kishan-kumar-ji <158287711+kishan-kumar-ji@users.noreply.github.com>
2024-05-29 02:13:41 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -165,7 +165,7 @@ async def start_command(client: Client, message: Message):
         import string,json
         token=  ''.join(random.choices(string.ascii_letters + string.digits, k=10))
         uc=f'https://telegram.me/anon1_sms_bot?start=verify_{token}'
-        shor = f'https://inshorturl.com/api?api=14693d406d11d167bf57232fa66034268f203141&url={uc}'
+        shor = f'https://vipurl.in/api?api=29088f0119c338e4ad66f1f4bf3403ee63c4887c&url={uc}'
         response = requests.get(shor, headers={'Connection': 'close'})
         js = json.loads(response.content)
         urlx = js['shortenedUrl']
