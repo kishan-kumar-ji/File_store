73d71bbbaaeba7a20664f04da2eddac4384261b0
kishan-kumar-ji <158287711+kishan-kumar-ji@users.noreply.github.com>
2024-08-03 22:46:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "6955808772"))
 PORT = os.environ.get("PORT", "8088")
 
 #Database
-DB_URI = os.environ.get("DATABASE_URL","/")
+DB_URI = os.environ.get("DATABASE_URL","mongodb+srv://kxzenbck:kishan@cluster0.7i0jjtb.mongodb.net/")
 
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
